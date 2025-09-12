dcd9fa53283b6a956bc864f7c08fd1a9d0fef4f4
gabrielms-castro <gabrielms.castro@gmail.com>
2025-09-12 16:28:44 -0300

    finished CH2:L2 assignment

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,30 @@
+import amqp from 'amqplib';
+import process from 'process';
+
 async function main() {
   console.log("Starting Peril server...");
+  
+  const connectionString = "amqp://guest:guest@localhost:5672/";
+  let connection
+
+  try {
+    connection = await amqp.connect(connectionString);
+
+    if (connection) {
+      console.log("Successfully connected to RabbitMQ!")
+    }
+
+  } catch (err) {
+    console.error("Failed to connect to RabbitMQ:", err);
+    process.exit(1);
+  }
+  
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+    await connection.close();
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
