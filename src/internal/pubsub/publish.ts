cae9dc1cea7818c90e78d3fe7facd463ea6a5201
gabrielms-castro <gabrielms.castro@gmail.com>
2025-09-14 09:21:48 -0300

    refactor publish.ts

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,6 +1,5 @@
 
-import type { ConfirmChannel, ChannelModel, Replies } from "amqplib";
-import type { Channel } from "diagnostics_channel";
+import { type ConfirmChannel } from "amqplib";
 
 export async function publishJSON<T>(
     ch: ConfirmChannel,
@@ -14,16 +13,6 @@ export async function publishJSON<T>(
         exchange,
         routingKey,
         buffer,
-        {contentType: "application/json"}
+        {contentType: "application/json"},
     );
-}
-
-export async function declareAndBin(
-    conn: ChannelModel,
-    exchange: string,
-    queueName: string,
-    key: string,
-    queueType: SimpleQueueType,
-): Promise<[Channel, Replies.AssertQueue]> {
-
 }
